7afbe9afe512e52f229fd2b2de8a23e8a4479a28
Kostyan89 <kostasamin@gmail.com>
2021-10-22 18:52:01 +0300

    correct mistakes in comments.json and write comments.json in app.route post

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
@@ -8,9 +9,12 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/post/<int:uid>')
-def posts_page():
-    return
+@app.route('/posts/<int:pk>')
+def posts_page(pk):
+    with open ('data.json', 'r') as f:
+        comments = json.load(f)
+
+    return render_template("post.html")
 
 
 app.run()
