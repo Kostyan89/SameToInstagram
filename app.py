fab0c271da808dc8f6dedd31807533573252b23e
Kostyan89 <kostasamin@gmail.com>
2021-10-22 19:23:23 +0300

    add right links for img's in every html file

--- app.py
+++ app.py
@@ -13,8 +13,15 @@ def index():
 def posts_page(pk):
     with open ('data.json', 'r') as f:
         comments = json.load(f)
+    return render_template("post.html", comments=comments)
 
-    return render_template("post.html")
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Страница не найдена, но всё не так  плохо как кажется! Выпей чаю и через 5 мин попробуй еще раз"
 
 
 app.run()
