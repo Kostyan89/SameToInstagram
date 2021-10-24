71b1dca430be881ff5674dd5113e15e1d908e2bf
Kostyan89 <kostasamin@gmail.com>
2021-10-24 21:22:44 +0300

    correct index.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-import json
+import requests
+from flask import Flask, render_template, request
 from functions import *
 app = Flask(__name__)
 
@@ -13,21 +13,21 @@ def index():
 
 @app.route('/posts/<int:post_id>')
 def page_index(post_id):
-    post = get_post_by_id(post_id)
+    posts = get_post_by_id(post_id)
     comments = get_comments_by_post_id(post_id)
-    return render_template('post.html', comments=comments, post=post)
+    return render_template('post.html', comments=comments, post=posts)
 
 
-@app.route('/search/s=<word>')
+@app.route('/search')
 def page_search(word):
+    data = request.args.get('s')
     posts = get_posts_by_word(word)
-    return render_template('search.html', posts=posts)
+    return render_template('search.html', posts=posts, data=data)
 
 
 @app.route('/users/<username>')
 def page_users(username):
     posts = get_posts_of_user(username)
-    print(posts)
     return render_template('user-feed.html', posts=posts)
 
 
@@ -37,4 +37,4 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
