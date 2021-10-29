0948c927773fbcb99a2038dc9e74f3920c087103
Kostyan89 <kostasamin@gmail.com>
2021-10-29 18:44:06 +0300

    correct mistakes

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def index():
 @app.route('/posts/<int:post_id>')
 def page_post(post_id):
     post = get_post_by_id(post_id)
+    if not post_id:
+        abort(404)
     comments = get_comments_by_post_id(post_id)
     return render_template('post.html', comments=comments, post=post)
 
@@ -28,13 +30,15 @@ def page_search():
 
 @app.route('/users/<username>')
 def page_users(username):
+    if not username:
+        abort(404)
     users_posts = get_posts_by_username(username)
     return render_template('user-feed.html', users_posts=users_posts)
-#
-#
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     return "Страница не найдена, но всё не так  плохо как кажется! Выпей чаю и через 5 мин попробуй еще раз"
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Страница не найдена, но всё не так  плохо как кажется! Выпей чаю и через 5 мин попробуй еще раз"
 
 
 if __name__ == '__main__':
